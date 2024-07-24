a1bac433f9887044e82a1858411e03f2da574c09
“emilesherrott” <emilesherrott@gmail.com>
2024-07-24 11:44:27 +0100

    Define two endpoints

--- api.js
+++ api.js
@@ -2,6 +2,8 @@
 const express = require("express")
 const cors = require("cors")
 
+const db = require("./database/db")
+
 // Defined middleware
 const logger = require("./logger")
 
@@ -13,6 +15,15 @@ api.use(express.json())
 api.use(cors())
 api.use(logger)
 
-api.get("/", (req, res) => res.send("The Wrongs API: track injustice"))
+api.get("/", async (req, res) => {
+    const data = await db.query("SELECT * FROM wrong;")
+    res.send(data.rows)
+})
+
+api.get("/people/:id", async (req, res) => {
+    const id = parseInt(req.params.id)
+    const data = await db.query("SELECT * FROM person WHERE person_id = $1", [id])
+    res.send(data.rows[0])
+})
 
 module.exports = api
