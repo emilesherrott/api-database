d3f03d317892376988d6638240cb11da5b5cac37
“emilesherrott” <emilesherrott@gmail.com>
2024-07-24 11:21:16 +0100

    Define DB Connection

---
+++ database/setup-db.js
@@ -0,0 +1,10 @@
+const fs = require("fs")
+require("dotenv").config()
+
+const sql = fs.readFileSync(__dirname + "/setup.sql").toString()
+
+const db = require("./db")
+
+db.query(sql)
+    .then(data => console.log("Set up complete"))
+    .catch(error => console.log(error))
